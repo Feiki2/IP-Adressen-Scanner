69b197058d260705378ec696799d9a4b7436b987
Feichtinger Jonas <J.Feichtinger@htlvb.at>
2026-04-10 08:48:16 +0200

    feat: add IP range iteration in RunScan()

--- Scanner.cs
+++ Scanner.cs
@@ -3,12 +3,29 @@
 public class Scanner 
 { 
     private bool PingHost(string ip) 
-
     { 
         using var ping = new Ping(); 
 
         var reply = ping.Send(ip, 500); 
 
         return reply.Status == IPStatus.Success; 
+    } 
+    // ERGAENZT in Scanner.cs: RunScan() Methode 
+
+    public List<ScanResult> RunScan() 
+    { 
+        var results = new List<ScanResult>(); 
+
+        uint start = IpToUint(_options.StartIp); 
+
+        uint end   = IpToUint(_options.EndIp); 
+
+        for (uint i = start; i <= end; i++) 
+        { 
+            string ip = UintToIp(i); 
+
+            results.Add(new ScanResult { IpAddress = ip, IsAlive = PingHost(ip) }); 
+        } 
+        return results; 
     } 
 } 
