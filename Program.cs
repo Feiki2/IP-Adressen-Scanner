576c4523dd32ca3b62096623ec90e95488bb60ea
Gappmaier Daniel <D.Gappmaier@htlvb.at>
2026-04-10 08:55:24 +0200

    feat: add CLI argument parsing in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Main(string[] args)
+{
+    var options = new ScanOptions { StartIp = "192.168.1.1", EndIp = "192.168.1.10" };
+    if (args.Length >= 2) { options.StartIp = args[0]; options.EndIp = args[1]; }
+    // Scan wird noch nicht aufgerufen - kommt im naechsten Commit
+}
